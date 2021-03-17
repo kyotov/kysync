9f5d599ef14939e7c91e41dfcfb4374232998452
Kamen Yotov <kamen@yotov.org>
2021-03-17 00:42:52 -0400

    simplify how dependencies are installed... no more shell files!

--- deps/rdrun.bat
+++
@@ -1,8 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-
-set TYPE=RelWithDebInfo
-
-mkdir %TYPE%
-chdir %TYPE%
-cmake -DCMAKE_BUILD_TYPE=%TYPE% --log-level=VERBOSE -G "NMake Makefiles" ..
-cmake --build . --target %1 --config %TYPE%
