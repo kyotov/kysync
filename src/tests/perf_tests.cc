bd992ee853650fc5a6e21ca2e0bc03ac55d7ed4f
Kamen Yotov <kamen@yotov.org>
2021-04-06 01:57:50 -0400

    performance tests with data collection

--- src/tests/perf_tests.cc
+++ src/tests/perf_tests.cc
@@ -1,8 +1,13 @@
+#include <glog/logging.h>
 #include <gtest/gtest.h>
+
+#include <chrono>
+#include <fstream>
 
 #include "../Monitor.h"
 #include "../commands/PrepareCommand.h"
 #include "../commands/SyncCommand.h"
+#include "../utilities/timer.h"
 #include "commands/GenDataCommand.h"
 #include "utilities/TempPath.h"
 
@@ -12,32 +17,146 @@ std::string GetFileUri(const fs::path &path) {
   return "file://" + path.string();
 }
 
-TEST(Performance, 1G_same) {  // NOLINT
-  auto tmp = TempPath(true);
+template <typename T>
+T GetEnv(const std::string &name, T default_value) {
+  auto env = std::getenv(name.c_str());
+  errno = 0;
+  auto value = env == nullptr ? default_value
+                              : static_cast<T>(strtoumax(env, nullptr, 10));
+  value = errno ? default_value : value;
+  LOG(INFO) << name << "=" << value;
+  return value;
+}
+
+struct Context {
+  fs::path path;
+  size_t data_size;
+  size_t seed_data_size;
+  size_t fragment_size;
+  size_t block_size;
+  int similarity;
+  int threads;
+  bool compression;
+  bool identity_reconstruction;
+
+  Context(const Context &) = default;
+
+  static Context Default() {
+    static const Context value{};
+    return value;
+  }
+
+private:
+  Context() {
+    auto root_path = std::getenv("TEST_ROOT_PATH");
+    path = root_path != nullptr ? root_path
+                                : std::filesystem::temp_directory_path();
+    LOG(INFO) << "TEST_ROOT_PATH=" << path;
+
+    data_size = GetEnv("TEST_DATA_SIZE", 1'000'000);
+    seed_data_size = GetEnv("TEST_SEED_DATA_SIZE", -1ULL);
+    fragment_size = GetEnv("TEST_FRAGMENT_SIZE", 123'456);
+    block_size = GetEnv("TEST_BLOCK_SIZE", 16'384);
+    similarity = GetEnv("TEST_SIMILARITY", 90);
+    threads = GetEnv("TEST_THREADS", 32);
+    compression = GetEnv("TEST_COMPRESSION", false);
+    identity_reconstruction = GetEnv("TEST_IDENTITY_RECONSTRUCTION", false);
+  }
+};
+
+#define PERFLOG(X) #X << "=" << X << std::endl
+
+void Execute(const Context context) {
+  const auto tmp = TempPath(false, context.path);
+  const fs::path kRootPath = tmp.GetPath();
+  const fs::path kDataPath = kRootPath / "data.bin";
+  const fs::path kSeedDataPath =
+      context.identity_reconstruction ? kDataPath : kRootPath / "seed_data.bin";
+  const fs::path kKysyncPath = kRootPath / "data.bin.kysync";
+  const fs::path kPzstPath = kRootPath / "data.bin.pzst";
+  const fs::path kOutPath = kRootPath / "data.bin.out";
+
+  std::ofstream log("perf.log", std::ios::app);
+
+  log << std::endl                        //
+      << PERFLOG(context.path)            //
+      << PERFLOG(context.data_size)       //
+      << PERFLOG(context.seed_data_size)  //
+      << PERFLOG(context.fragment_size)   //
+      << PERFLOG(context.block_size)      //
+      << PERFLOG(context.similarity)      //
+      << PERFLOG(context.threads)         //
+      << PERFLOG(context.compression)     //
+      << PERFLOG(context.identity_reconstruction);
 
   auto gen_data = GenDataCommand(  //
       tmp.GetPath(),
-      1'000'000,
-      -1ULL,
-      123'456,
-      90);
-  EXPECT_EQ(Monitor(gen_data).Run(), 0);
+      context.data_size,
+      context.seed_data_size,
+      context.fragment_size,
+      context.similarity);
+  auto m_gen_data = Monitor(gen_data);
+  EXPECT_EQ(m_gen_data.Run(), 0);
+  m_gen_data.MetricSnapshot([&log](std::string key, auto value) {
+    if (key.starts_with("//phases")) {
+      log << "gen_data:" << key << "=" << value << std::endl;
+    }
+  });
 
   auto prepare = PrepareCommand(  //
-      tmp.GetPath() / "data.bin",
-      "",
-      "",
-      16'384);
-  EXPECT_EQ(Monitor(prepare).Run(), 0);
+      kDataPath,
+      kKysyncPath,
+      kPzstPath,
+      context.block_size);
+  auto m_prepare = Monitor(prepare);
+  EXPECT_EQ(m_prepare.Run(), 0);
+  m_prepare.MetricSnapshot([&log](auto key, auto value) {
+    if (key.starts_with("//phases")) {
+      log << "prepare:" << key << "=" << value << std::endl;
+    }
+  });
 
   auto sync = SyncCommand(  //
-      GetFileUri(tmp.GetPath() / "data.bin"),
-      GetFileUri(tmp.GetPath() / "data.bin.kysync"),
-      GetFileUri(tmp.GetPath() / "data.bin"),
-      tmp.GetPath() / "output.bin",
-      true,
-      32);
-  EXPECT_EQ(Monitor(sync).Run(), 0);
+      GetFileUri(kDataPath),
+      GetFileUri(kKysyncPath),
+      GetFileUri(kSeedDataPath),
+      kOutPath,
+      !context.compression,
+      context.threads);
+  auto m_sync = Monitor(sync);
+  EXPECT_EQ(m_sync.Run(), 0);
+  m_sync.MetricSnapshot([&log](auto key, auto value) {
+    if (key.starts_with("//phases")) {
+      log << "sync:" << key << "=" << value << std::endl;
+    }
+  });
+};
+
+TEST(Performance, Simple) {  // NOLINT
+  auto ctx = Context::Default();
+  Execute(ctx);
+}
+
+TEST(Performance, Detect) {  // NOLINT
+  // NOTE: the default is artificially low so as not to eat time on the github.
+  //       in real life, for performance testing we would want ~30s here.
+  size_t target_ms = GetEnv("TEST_TARGET_MS", 1'000);
+
+  auto ctx = Context::Default();
+  ctx.identity_reconstruction = true;
+
+  for (;;) {
+    auto beg = Now();
+    Execute(ctx);
+    auto end = Now();
+    if (DeltaMs(beg, end) > target_ms) {
+      break;
+    }
+    ctx.data_size *=
+        std::max(2ULL, std::min(16ULL, target_ms / DeltaMs(beg, end)));
+  }
+
+  LOG(INFO) << "detected test size = " << ctx.data_size;
 }
 
 }  // namespace kysync
