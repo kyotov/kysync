69b34e6d624ddcaf76fc330f039e12808cd306b9
Kamen Yotov <kamen@yotov.org>
2021-04-11 13:30:15 -0400

    proposed fix of temp_path

--- src/tests/utilities/temp_path.cc
+++ src/tests/utilities/temp_path.cc
@@ -1,7 +1,8 @@
 #include "temp_path.h"
 
-#include "glog/logging.h"
 #include <cstdlib>
+
+#include "glog/logging.h"
 
 namespace kysync {
 
@@ -9,25 +10,22 @@ namespace fs = std::filesystem;
 
 struct TempPath::Impl {
   const bool kKeep;
-  const fs::path kRootPath;
-  fs::path temp_path_;
+  const fs::path kPath;
 };
 
-TempPath::TempPath(bool keep, const std::filesystem::path root)
-    : impl_(new Impl{.kKeep = keep, .kRootPath = root}) {
-  char dir_name_template[] = "ksync_files_test_XXXXXX";
-  impl_->temp_path_ = root / mkdtemp(dir_name_template);
-  CHECK(!fs::exists(impl_->temp_path_))
-      << "Unexpected existence of temp dir " << impl_->temp_path_;
-  fs::create_directories(impl_->temp_path_);
+TempPath::TempPath(bool keep, const std::filesystem::path &path)
+    : impl_(new Impl{.kKeep = keep, .kPath = path / tmpnam(nullptr)}) {
+  CHECK(!fs::exists(impl_->kPath))
+      << "temporary path " << impl_->kPath << "already exists";
+  fs::create_directories(impl_->kPath);
 }
 
 TempPath::~TempPath() {
   if (!impl_->kKeep) {
-    fs::remove_all(impl_->temp_path_);
+    fs::remove_all(impl_->kPath);
   }
 }
 
-std::filesystem::path TempPath::GetPath() const { return impl_->temp_path_; }
+std::filesystem::path TempPath::GetPath() const { return impl_->kPath; }
 
 }  // namespace kysync
