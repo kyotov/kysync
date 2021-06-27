83c15324a189bc60d8eb4b94a7a42f79f950f20d
Kamen Yotov <kamen@yotov.org>
2021-06-27 00:24:52 -0400

    symlinks test

---
+++ kylibs/parallelize/include/parallelize.h
@@ -0,0 +1,18 @@
+#ifndef KSYNC_SRC_KY_PARALLELIZE_INCLUDE_KY_PARALLELIZE_H
+#define KSYNC_SRC_KY_PARALLELIZE_INCLUDE_KY_PARALLELIZE_H
+
+#include <functional>
+
+namespace ky::parallelize {
+
+int Parallelize(
+    std::streamsize data_size,
+    std::streamsize block_size,
+    std::streamsize overlap_size,
+    int threads,
+    const std::function<
+        void(int /*id*/, std::streamoff /*beg*/, std::streamoff /*end*/)> &f);
+
+}
+
+#endif  // KSYNC_SRC_KY_PARALLELIZE_INCLUDE_KY_PARALLELIZE_H
